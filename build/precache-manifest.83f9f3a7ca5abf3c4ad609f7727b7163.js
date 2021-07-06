bc6a56ae34cd73d6ab3eb4dd071cf54d4d2ec6a6
Rajani-Surarapu <rajanisurarapu@gmail.com>
2021-07-06 12:56:06 +0530

    first commit

---
+++ build/precache-manifest.83f9f3a7ca5abf3c4ad609f7727b7163.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d3b7736032c2ffafea8",
+    "url": "/static/css/main.6a0a28a1.chunk.css"
+  },
+  {
+    "revision": "5d3b7736032c2ffafea8",
+    "url": "/static/js/main.5d3b7736.chunk.js"
+  },
+  {
+    "revision": "f608697f507c53c2a042",
+    "url": "/static/js/1.f608697f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "c4bcad60fb647651bcac8dce2f92cc6f",
+    "url": "/index.html"
+  }
+];
